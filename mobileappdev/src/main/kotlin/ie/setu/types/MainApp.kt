907bcb3da2a06502e20aa00f438c378875ee3232
JHolland <20102340@mail.wit.ie>
2025-09-18 18:32:36 +0100

    Character types

--- mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
+++ mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
@@ -3,16 +3,21 @@ package org.example.ie.setu.types
 fun main(){
     println("Hello World !")
     println("Working with Variables and Types")
+
     println("doubleNumber value is: " + doubleNumber + ", and the type is: " + doubleNumber.javaClass)
     println("floatNumber value is:  " + floatNumber + ", and the type is: " + floatNumber.javaClass)
     println("longNumber value is:   " + longNumber + ", and the type is: " + longNumber.javaClass)
     println("intNumber value is:    " + intNumber + ", and the type is: " + intNumber.javaClass)
     println("shortNumber value is:  " + shortNumber + ", and the type is: " + shortNumber.javaClass)
     println("byteNumber value is:   " + byteNumber + ", and the type is: " + byteNumber.javaClass)
+
     println("\nUsing underscores to make values more readable:")
     println("\t" + oneMillion + " - the type is: " + oneMillion.javaClass)
     println("\t" + threeThousand + " - the type is: " + threeThousand.javaClass)
     println("\t" + creditCardNumber + " - the type is: " + creditCardNumber.javaClass)
+
+    println("aChar: " + aChar + " - the type is: " + aChar.javaClass)
+    println("bChar: " + bChar + " - the type is: " + bChar.javaClass)
 
 }
 val doubleNumber = 1200.45
@@ -34,3 +39,6 @@ val intNum: Int = byteNum.toInt()   //explicit conversion: OK
 val floatNum: Float = byteNum.toFloat()
 val doubleNum: Double = intNum.toDouble()
 
+//characters in kotlin
+val aChar = 'a'			//type inference
+val bChar: Char = 'b'   //explicitly defining the type
