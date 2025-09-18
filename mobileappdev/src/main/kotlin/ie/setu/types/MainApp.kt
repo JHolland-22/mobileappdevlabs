0b1564de78e899029d677149f611162760f445a4
JHolland <20102340@mail.wit.ie>
2025-09-18 18:18:03 +0100

    Values with Underscores

--- mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
+++ mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
@@ -9,6 +9,10 @@ fun main(){
     println("intNumber value is:    " + intNumber + ", and the type is: " + intNumber.javaClass)
     println("shortNumber value is:  " + shortNumber + ", and the type is: " + shortNumber.javaClass)
     println("byteNumber value is:   " + byteNumber + ", and the type is: " + byteNumber.javaClass)
+    println("\nUsing underscores to make values more readable:")
+    println("\t" + oneMillion + " - the type is: " + oneMillion.javaClass)
+    println("\t" + threeThousand + " - the type is: " + threeThousand.javaClass)
+    println("\t" + creditCardNumber + " - the type is: " + creditCardNumber.javaClass)
 
 }
 val doubleNumber = 1200.45
@@ -17,3 +21,10 @@ val longNumber = 98765432L
 val intNumber = 123456
 val shortNumber = 345
 val byteNumber = 127
+
+
+val oneMillion = 1_000_000
+val threeThousand = 3_000
+val creditCardNumber = 1234_4321_5678_8765
+
+
