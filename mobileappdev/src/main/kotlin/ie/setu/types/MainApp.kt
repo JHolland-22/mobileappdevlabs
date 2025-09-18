bfbebea4ab5a02be98a617d1f1473ae4506bd4d1
JHolland <20102340@mail.wit.ie>
2025-09-18 18:14:25 +0100

    Basic Types (no type inference)

--- mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
+++ mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
@@ -3,4 +3,17 @@ package org.example.ie.setu.types
 fun main(){
     println("Hello World !")
     println("Working with Variables and Types")
+    println("doubleNumber value is: " + doubleNumber + ", and the type is: " + doubleNumber.javaClass)
+    println("floatNumber value is:  " + floatNumber + ", and the type is: " + floatNumber.javaClass)
+    println("longNumber value is:   " + longNumber + ", and the type is: " + longNumber.javaClass)
+    println("intNumber value is:    " + intNumber + ", and the type is: " + intNumber.javaClass)
+    println("shortNumber value is:  " + shortNumber + ", and the type is: " + shortNumber.javaClass)
+    println("byteNumber value is:   " + byteNumber + ", and the type is: " + byteNumber.javaClass)
+
 }
+val doubleNumber: Double = 1200.45
+val floatNumber: Float = 700.78f
+val longNumber: Long = 98765432
+val intNumber: Int = 123456
+val shortNumber: Short = 345
+val byteNumber: Byte = 127
