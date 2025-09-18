3e84c58d38e227cd97c7aec47d7c0f102d3aa2a4
JHolland <20102340@mail.wit.ie>
2025-09-18 18:49:41 +0100

    Factory Reset git add . - Your First function without parameters

--- mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
+++ mobileappdev/src/main/kotlin/ie/setu/types/MainApp.kt
@@ -1,8 +1,22 @@
 package org.example.ie.setu.types
 
-fun main(){
-    println("Hello World !")
-    println("Working with Variables and Types")
+fun main() {
+
+        typeInference()
+
+}
+
+fun typeInference() {
+    println ("\n==========================================================")
+    println ("============  Using Type Inference in Kotlin =============")
+    println ("==========================================================")
+
+    val doubleNumber = 1200.45
+    val floatNumber = 700.78f
+    val longNumber = 98765432L
+    val intNumber = 123456
+    val shortNumber = 345
+    val byteNumber = 127
 
     println("doubleNumber value is: " + doubleNumber + ", and the type is: " + doubleNumber.javaClass)
     println("floatNumber value is:  " + floatNumber + ", and the type is: " + floatNumber.javaClass)
@@ -11,52 +25,6 @@ fun main(){
     println("shortNumber value is:  " + shortNumber + ", and the type is: " + shortNumber.javaClass)
     println("byteNumber value is:   " + byteNumber + ", and the type is: " + byteNumber.javaClass)
 
-    println("\nUsing underscores to make values more readable:")
-    println("\t" + oneMillion + " - the type is: " + oneMillion.javaClass)
-    println("\t" + threeThousand + " - the type is: " + threeThousand.javaClass)
-    println("\t" + creditCardNumber + " - the type is: " + creditCardNumber.javaClass)
-
-    //bChar = 'B'
-    //println("aChar: " + aChar + " - the type is: " + aChar.javaClass)
-    //println("bChar: " + bChar + " - the type is: " + bChar.javaClass)
-
-
-    println("aFlag: " + aFlag + " - the type is: " + aFlag.javaClass)
-    println("bFlag: " + bFlag + " - the type is: " + bFlag.javaClass)
-
-    number = 3      //mathematical operations are allowed
-    println("number: " + number)
-
 
 }
-val doubleNumber = 1200.45
-val floatNumber = 700.78f
-val longNumber = 98765432L
-val intNumber = 123456
-val shortNumber = 345
-val byteNumber = 127
 
-
-val oneMillion = 1_000_000
-val threeThousand = 3_000
-val creditCardNumber = 1234_4321_5678_8765
-
-
-val byteNum: Byte = 10              //static type check: OK
-val intNum: Int = byteNum.toInt()   //explicit conversion: OK
-
-val floatNum: Float = byteNum.toFloat()
-val doubleNum: Double = intNum.toDouble()
-
-//bChar = 'B'
-//characters in kotlin
-//val aChar = 'a'			//type inference
-//val bChar: Char      //explicitly defining the type - no value set
-
-
-//booleans in kotlin
-val aFlag = true
-val bFlag: Boolean = false
-
-//mutable variable
-var number = 5   //Int type is inferred
